b2d0ca91621c105711f76dd3b6aab6072015a7b1
Hakima-Muktar <hakimamuktar73@gmail.com>
2026-01-12 12:10:21 +0300

    commit

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ with gr.Blocks() as demo:
 if __name__ == "__main__":
     demo.launch(
         share=False, 
-        server_name="0.0.0.0", 
+        server_name="127.0.0.1", 
         server_port=7860,
         show_error=True,
         theme=gr.themes.Soft()
